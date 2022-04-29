b6fdcec2d3c5335910d8756995682f57d9b87628
Justin Clark <justin.clark@climate.com>
2022-04-29 02:58:26 -0500

    Add the missing Lambda templates

--- software/GetFileFunction/src/main/java/function/GetFile.java
+++ software/GetFileFunction/src/main/java/function/GetFile.java
@@ -1,3 +1,5 @@
+package function;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
